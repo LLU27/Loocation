05a62466e9d53ccd6703029e585709716e255195
Le <alanre27@gmail.com>
2024-09-18 13:13:36 -0400

    setup before each knowngoodstate

--- Backend/Loocation/src/test/java/learn/loocation/data/AddressJdbcTemplateRepositoryTest.java
+++ Backend/Loocation/src/test/java/learn/loocation/data/AddressJdbcTemplateRepositoryTest.java
@@ -1,13 +1,29 @@
 package learn.loocation.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class AddressJdbcTemplateRepositoryTest {
+
+    @Autowired
+    AddressJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
 
     @Test
     void findAddressById() {
+
     }
 
     @Test
--- Backend/Loocation/src/test/java/learn/loocation/data/RatingJdbcTemplateRepositoryTest.java
+++ Backend/Loocation/src/test/java/learn/loocation/data/RatingJdbcTemplateRepositoryTest.java
@@ -1,10 +1,23 @@
 package learn.loocation.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RatingJdbcTemplateRepositoryTest {
+    @Autowired
+    RatingJdbcTemplateRepository repository;
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
 
     @Test
     void findByRatingId() {
