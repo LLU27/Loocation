95e264f67bab7fcf2deb17e6115b8d5e0c4c4afc
Le <alanre27@gmail.com>
2024-09-19 13:26:00 -0400

    finished rating and address service test

--- Backend/Loocation/src/test/java/learn/loocation/domain/BathroomServiceTest.java
+++ Backend/Loocation/src/test/java/learn/loocation/domain/BathroomServiceTest.java
@@ -1,28 +1,118 @@
 package learn.loocation.domain;
 
+import learn.loocation.data.BathroomRepository;
+import learn.loocation.models.Address;
+import learn.loocation.models.Bathroom;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class BathroomServiceTest {
+@Autowired
+BathroomService service;
+@MockBean
+BathroomRepository repository;
+
+
+    Address address = new Address(1, "123 Test St", "Test City", "TS", "12345", 43, 43);
+    Address address2 = new Address(2, "321 Test St", "Test City", "TS", "12345", 43, 43);
 
     @Test
-    void findAllBathrooms() {
+    void shouldFindAllBathrooms() {
+        Bathroom bathroom1 = new Bathroom(1, "test bathroom", true, true, true, address);
+        Bathroom bathroom2 = new Bathroom(2, "test bathroom", true, true, true, address2);
+        List<Bathroom> expected = List.of(bathroom1, bathroom2);
+        when(repository.findAllBathrooms()).thenReturn(expected);
+        List<Bathroom> actual = service.findAllBathrooms();
+        assertEquals(expected, actual);
+        assertEquals(2, actual.size());
     }
 
     @Test
-    void findBathroomById() {
+    void shouldFindBathroomById() {
+        Bathroom expected = new Bathroom(1,"test bathroom",true, true, true, address);
+        when(repository.findBathroomById(1)).thenReturn(expected);
+        assertEquals(expected, service.findBathroomById(1));
     }
 
     @Test
-    void addBathroom() {
+    void shouldAddBathroom() {
+        Bathroom expected = new Bathroom(1,"test bathroom",true, true, true, address);
+        Bathroom actual = new Bathroom(1,"test bathroom",true, true, true, address);
+        Result<Bathroom> expectedResult = new Result<>();
+        expectedResult.setPayload(expected);
+        when(repository.addBathroom(actual)).thenReturn(expected);
+        Result<Bathroom> actualResult = service.addBathroom(actual);
+        assertEquals(expected, actualResult.getPayload());
     }
 
     @Test
-    void updateBathroom() {
+    void shouldNotAddNull() {
+        Bathroom bathroom = new Bathroom();
+        Result<Bathroom> result = service.addBathroom(bathroom);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
-    void deleteBathroomById() {
+    void shouldNotAddEmpty() {
+        Bathroom bathroom = new Bathroom(0, "", false, false, false, address);
+        Result<Bathroom> result = service.addBathroom(bathroom);
+        assertEquals(ResultType.INVALID, result.getType());
     }
+
+    @Test
+    void shouldNotAddNullName() {
+        Bathroom bathroom = new Bathroom(0, null, false, false, false, address);
+        Result<Bathroom> result = service.addBathroom(bathroom);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+
+    @Test
+    void shouldUpdateBathroom() {
+        Bathroom existing = new Bathroom(1,"test bathroom",true, true, true, address);
+        when(repository.updateBathroom(existing)).thenReturn(true);
+        assertTrue(repository.updateBathroom(existing));
+    }
+    @Test
+    void shouldNotUpdateWhenInvalid() {
+        Bathroom bathroom = new Bathroom();
+        Result<Bathroom> actual = service.updateBathroom(bathroom);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenInvalidName() {
+        Bathroom bathroom = new Bathroom(1, "", false, false, false, address);
+        Result<Bathroom> actual = service.updateBathroom(bathroom);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+    @Test
+    void shouldNotDeleteWhenInvalid() {
+        when(repository.deleteBathroomById(0)).thenReturn(false);
+        Result<Boolean> result = service.deleteBathroomById(0);
+        assertFalse(result.getPayload());
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldDeleteBathroomById() {
+        when(repository.deleteBathroomById(1)).thenReturn(true);
+        when(repository.deleteBathroomById(999)).thenReturn(false);
+
+        Result<Boolean> result1 = service.deleteBathroomById(1);
+        Result<Boolean> result999 = service.deleteBathroomById(999);
+
+        assertTrue(result1.getPayload());
+        assertFalse(result999.getPayload());
+    }
+
+
 }
--- Backend/Loocation/src/test/java/learn/loocation/domain/RatingServiceTest.java
+++ Backend/Loocation/src/test/java/learn/loocation/domain/RatingServiceTest.java
@@ -1,20 +1,103 @@
 package learn.loocation.domain;
 
+import learn.loocation.data.RatingRepository;
+import learn.loocation.models.Rating;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RatingServiceTest {
+    @Autowired
+    RatingService service;
+
+    @MockBean
+    RatingRepository repository;
+
 
     @Test
-    void findRatingById() {
+    void shouldFindRatingById() {
+        Rating expected = new Rating(1, 1, 1, 5, "Great place to visit");
+        when(repository.findByRatingId(1)).thenReturn(expected);
+        Rating actual = service.findRatingById(1);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void addRating() {
+    void shouldAddRating() {
+        Rating expected = new Rating(1, 1, 1, 5, "Great place to visit");
+        Rating actual = new Rating(0, 1, 1, 5, "Great place to visit");
+        when(repository.addRating(actual)).thenReturn(expected);
+        Rating result = service.addRating(actual).getPayload();
+        assertEquals(expected, result);
+        assertNotNull(result);
+        assertEquals(1, result.getRatingId());
     }
 
     @Test
-    void updateRating() {
+    void shouldNotAddNull() {
+        Rating rating = new Rating();
+        Result<Rating> result = service.addRating(rating);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotAddEmpty() {
+        Rating rating = new Rating(0, 0, 0, 0, "");
+        Result<Rating> result = service.addRating(rating);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotAddWhenInvalid() {
+        Rating rating = new Rating(0, 0, 0, 0, "");
+        Result<Rating> result = service.addRating(rating);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotAddWhenInvalidRating() {
+        Rating rating = new Rating(0, 0, 0, 6, "Great place to visit");
+        Result<Rating> result = service.addRating(rating);
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().contains("rating must be between 1 and 5"));
+    }
+
+    @Test
+    void shouldUpdateRating() {
+        Rating original = new Rating(1, 1, 1, 5, "Great place to visit");
+        Rating updated = new Rating(1, 1, 1, 1, "Updated comment");
+        when(repository.findByRatingId(1)).thenReturn(original);
+        when(repository.updateRating(updated)).thenReturn(true);
+        Result<Rating> result = service.updateRating(updated);
+        assertTrue(result.isSuccess());
+        assertEquals("Updated comment", updated.getComment());
+        assertEquals(1, updated.getRating());
+    }
+    @Test
+    void shouldNotUpdateWhenInvalid() {
+        Rating rating = new Rating();
+        Result<Rating> actual = service.updateRating(rating);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenInvalidRating() {
+        Rating rating = new Rating(1, 1, 1, 6, "Great place to visit");
+        Result<Rating> result = service.updateRating(rating);
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().contains("rating must be between 1 and 5"));
+    }
+
+    @Test
+    void shouldNotUpdateWhenNotFound() {
+        Rating rating = new Rating(1, 1, 1, 5, "Great place to visit");
+        when(repository.findByRatingId(1)).thenReturn(null);
+        Result<Rating> result = service.updateRating(rating);
+        assertEquals(ResultType.NOT_FOUND, result.getType());
     }
 }
